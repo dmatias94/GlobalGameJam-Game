7014a752cf05301962cb61e85f94288bd4f196c3
David Matias <dmatias@mail.bradley.edu>
2019-01-26 15:13:38 -0600

    com

---
+++ GGJ_Project/Assets/LaserBehavior.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehavior : MonoBehaviour
+{
+
+    public Transform m_muzzle;
+	public GameObject m_shotPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GameObject go = GameObject.Instantiate(m_shotPrefab, m_muzzle.position, m_muzzle.rotation) as GameObject;
+		GameObject.Destroy(go, 3f);
+    }
+}
